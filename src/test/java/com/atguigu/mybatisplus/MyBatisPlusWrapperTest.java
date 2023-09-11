104c312dd77ec63e5bccdbafd281af77351e047f
jiangyang <591277686@qq.com>
2023-09-11 22:23:20 +0800

    4commit

--- src/test/java/com/atguigu/mybatisplus/MyBatisPlusWrapperTest.java
+++ src/test/java/com/atguigu/mybatisplus/MyBatisPlusWrapperTest.java
@@ -1,4 +1,47 @@
 package com.atguigu.mybatisplus;
 
+import com.atguigu.mybatisplus.mapper.UserMapper;
+import com.atguigu.mybatisplus.pojo.User;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
 public class MyBatisPlusWrapperTest {
+    @Autowired
+    private UserMapper userMapper;
+    @Test
+    public void test01(){
+
+        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
+        queryWrapper.like("name","a")
+                        .between("age",20,30)
+                                .isNotNull("email");
+        List<User> list = userMapper.selectList(queryWrapper);
+        list.forEach(System.out::println);
+    }
+    @Test
+    public void test02(){
+
+        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
+        queryWrapper.orderByDesc("age").orderByAsc("id");
+        List<User> list = userMapper.selectList(queryWrapper);
+        list.forEach(System.out::println);
+
+    }
+    @Test
+    public void test03(){
+        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
+        queryWrapper.isNull("email");
+        int result = userMapper.delete(queryWrapper);
+        System.out.println("result："+result);
+
+
+
+    }
+
+
 }
